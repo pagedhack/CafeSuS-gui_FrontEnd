2fc0f05a12001fe3310bf4d4eb516dce18afe93a
pagedhack <samu1@outlook.es>
2022-12-11 21:36:26 -0600

    Proyecto

--- cafeteria/log/js/res.js
+++ cafeteria/log/js/res.js
@@ -1,7 +1,7 @@
         var bConsultar = document.getElementById("consultar")
 
         bConsultar.addEventListener("click", function () {
-            axios.get("https://cafesus-despliegue-production-1e7c.up.railway.app/usuario")
+            axios.get("https://cafesus-despliegue-production-1e7c.up.railway.app/reservaciones")
                 .then(function (response) {
                     console.log(response);
                     tabla(response.data)
@@ -15,12 +15,10 @@
             const myObj = JSON.parse(JSON.stringify(response));
             console.log(myObj)
             let text = "<table class='table'><tbody>"
-                text += "<thead><th>ID</th><th>Usuario</th><th>Password</th><th>nombre</th><th>correo</th></thead>"
+                text += "<thead><th>ID</th><th>Fecha</th><th>Hora</th><th>Personas</th><th>Nombre</th><th>Correo</th><th>Telefono</th></thead>"
             for (let x in myObj) {
-                text += "<tr><td>" + myObj[x].id + "</td><td>" + myObj[x].nombre + "</td><td>" + myObj[x].password + "</td><td>" + myObj[x].nombre + "</td><td>" + myObj[x].correo + "</td></td>";
+                text += "<tr><td>" + myObj[x].id + "</td><td>" + myObj[x].fecha + "</td><td>" + myObj[x].hora + "</td><td>" + myObj[x].persona + "</td><td>" + myObj[x].nombre + "</td><td>" + myObj[x].correo + "</td><td>" + myObj[x].telefono + "</td></td>";
             }
             text += "</tbody></table>"    
             document.getElementById("usuarios").innerHTML = text;
         }
-    
-        
